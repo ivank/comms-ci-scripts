3a2e35fc5db31589c80550d81cfbb2aafbf76b18
Steve <steve.fleetwood@ovoenergy.com>
2016-11-11 17:48:41 +0000

    Do not tag latest in Docker

--- publish_docker_image.sh
+++ publish_docker_image.sh
@@ -16,6 +16,6 @@ git_sha1="${CIRCLE_SHA1:-$(git rev-parse HEAD)}"
 echo Publishing docker image to ECR with version $git_sha1
 
 # Assume sbt is in charge of logging in to ECR
-sbt "; set version := \"$git_sha1\"; set dockerUpdateLatest := true; docker:publish"
+sbt "; set version := \"$git_sha1\"; docker:publish"
 
 
